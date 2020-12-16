2e429cba521b10c28a13eb5d4fb657fed138a301
zenopinto <73061542+zenopinto@users.noreply.github.com>
2020-12-16 18:45:13 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box {
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height)
+        this.width=width
+        this.height=height
+        World.add(world,this.body)
+    }
+    display(){
+        var pos =this.body.position
+        var angle=this.body.angle
+        push();
+        translate(pos.x,pos.y)
+        rotate(angle)
+        rectMode(CENTER)
+        fill(rgb(255,0,0))
+        rect(0,0,this.width,this.height)
+        pop();
+    }
+}
